0d3114175921186bbd5a4d58776fe3b3a1130231
Jason Dean <jasondeanfitness@outlook.com>
2026-08-04 21:04:51 +0100

    fix: restore Mapbox basemap

--- src/components/georisk/maps/RiskMap.tsx
+++ src/components/georisk/maps/RiskMap.tsx
@@ -105,7 +105,8 @@ export const RiskMap: React.FC<RiskMapProps> = ({ lanes, scores }) => {
                 {...viewState}
                 onMove={(evt: any) => setViewState(evt.viewState)}
                 style={{ width: '100%', height: '100%' }}
-                mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+                mapStyle="mapbox://styles/mapbox/light-v11"
+                mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
                 interactiveLayerIds={['risk-corridors', 'vessel-markers']}
                 onMouseMove={onHover}
             >
